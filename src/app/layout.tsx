120ef1f999d45f936a2f5ea8310f0a89bc8ed535
채지원 <dev.julia.chae@gmail.com>
2025-09-06 01:21:33 +0900

    Feat: seo 최적화를 위해 관련 파일 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,33 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://julia98percent.me"),
   title: "치악산 복숭아",
   description: "치악산에서 자생하는 복숭아입니다.",
+  openGraph: {
+    siteName: "치악산 복숭아",
+    type: "website",
+    url: "/",
+    images: [
+      {
+        url: "/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "치악산 복숭아",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "치악산 복숭아",
+    description: "치악산에서 자생하는 복숭아입니다.",
+    images: [
+      {
+        url: "/thumbnail.png",
+        alt: "치악산 복숭아",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
